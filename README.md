10028f0f82c8eb339d183d6d1970da3e5fe5307d
tmytokai <tmytokai@yahoo.co.jp>
2021-04-29 19:01:36 +0900

    初期ファイル追加

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # statemachine
 2年実験の状態遷移の説明用
+
+https://tmytokai.github.io/statemachine/
+
+インストール
+
+    $ npm install --save-dev babel-loader @babel/core @babel/preset-env @babel/preset-react @babel/plugin-proposal-class-properties
+    $ npm install --save-dev react react-dom
+    $ npm install --save-dev style-loader css-loader
+    $ npm install --save-dev webpack webpack-cli webpack-dev-server
+    $ npm install --save-dev gh-pages
+    $ npm install --save-dev vis
+
+実行
+
+    $ npm start
+
+デプロイ
+
+    $ npm run build
+    $ npm run deploy
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        path: __dirname+"/dist",
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: [
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/preset-env', '@babel/preset-react' ],
+                        plugins: [ '@babel/plugin-proposal-class-properties' ]
+                    },
+                }
+             ]
+          },
+         {
+             test: /\.css$/,
+             exclude: /node_modules/,
+             use: [
+                 "style-loader",
+                 {
+                     loader: "css-loader",
+                     options: { url: false }
+                 }
+             ]
+          }
+       ]
+   }
+};
